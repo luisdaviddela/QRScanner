6a347dba4f90b0b0d93c1f47ae80371d2510cf6e
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-12-05 10:42:07 -0600

    ZXing

--- QRScan/QRScan/QRScan.Android/MainActivity.cs
+++ QRScan/QRScan/QRScan.Android/MainActivity.cs
@@ -21,5 +21,9 @@ namespace QRScan.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
--- QRScan/QRScan/QRScan/MainPage.xaml.cs
+++ QRScan/QRScan/QRScan/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
 
 namespace QRScan
 {
@@ -12,6 +13,31 @@ namespace QRScan
         public MainPage()
         {
             InitializeComponent();
+        }
+        private void OpenScanner(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        public async void Scanner()
+        {
+
+            var ScannerPage = new ZXingScannerPage();
+
+            ScannerPage.OnScanResult += (result) => {
+                // Parar de escanear
+                ScannerPage.IsScanning = false;
+
+                // Alert com o código escaneado
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Código escaneado", result.Text, "OK");
+                });
+            };
+
+
+            await Navigation.PushAsync(ScannerPage);
+
         }
     }
 }
